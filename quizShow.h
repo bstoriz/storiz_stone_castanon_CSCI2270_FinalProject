197d1fe61dfa137394350c1e62002a78a552fdfe
Brandon Stone <brandonn.stone@gmail.com>
2016-04-23 11:54:32 -0600

    added comments

--- quizShow.h
+++ quizShow.h
@@ -2,12 +2,20 @@
 #define QUIZSHOW_H
 #include <string>
 
+/*
+Right now each quiz topic has their own .txt files. the information is organized as shown below
+string question,string answer choice1,string answer choice2,string answer choice 3,int correct answer
+
+the idea is to read in the appropriate .txt file for a chosen quiz topic and use a comma delimiter to separate the info into the struct shown in this file.
+the program will print the question and three answers and compare the user input with what is in the last slot of the .txt file.
+*/
+
 struct topicQuestion{
-    std::string question;
-    std::string answer1;
-    std::string answer2;
-    std::string answer3;
-    int correctAnswer;
+    std::string question; //read in from file and printed
+    std::string answer1; //read in from file and printed
+    std::string answer2; //read in from file and printed
+    std::string answer3; //read in from file and printed
+    int correctAnswer; //read in from file and compared to user input
 };
 
 class quizShow
@@ -22,7 +30,8 @@ class quizShow
     protected:
 
     private:
-        int numberOfCorrect = 0;
+        int totalNumberOfQuestions = 0; //initialized to 0 and will increment as questions are displayed
+        int numberOfCorrect = 0; //initialized to 0 and will increment as the questions are deleted from the linked list
 };
 
 #endif // QUIZSHOW_H
