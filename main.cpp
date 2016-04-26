38e021645618b392fbc9720b5412d26ce5d82126
caca1257 <caca1257@colorado.edu>
2016-04-25 20:05:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,28 +21,52 @@ int main(){
         cout << "====== Welcome ======" << endl << endl; //standard welcome banner
         cout << "Choose a number to take the corresponding quiz" << endl; //user prompt
         //initial ideas for quiz topics. i honestly have no ideas what will be "fun" to quiz on
-        cout << "1. History" << endl;
-        cout << "2. Math" << endl;
-        cout << "3. Science" << endl;
-        cout << "4. Random Trivia" << endl;
-        cout << "5. Quit" << endl;
+        cout << "1. Art" << endl;
+        cout << "2. English" << endl;
+        cout << "3. Computer Science" << endl;
+        cout << "4. Physics" << endl;
+        cout << "5. Chemistry" << endl;
+        cout << "6. Geography" << endl;
+        cout << "7. History" << endl;
+        cout << "8. Math" << endl;
+        cout << "9. Science" << endl;
+        cout << "10. Random Trivia" << endl;
+        cout << "11". Quit" << endl;
         cin >> mainMenuChoice;
         cout << endl;
         cout << endl;
         cout << endl;
         if(mainMenuChoice == 1){
-            qs->topicHistory();
+            qs->topicArt();
         }
         if(mainMenuChoice == 2){
-            qs->topicMath();
+            qs->topicEnglish();
         }
         if(mainMenuChoice == 3){
-            qs->topicScience();
+            qs->topicCompSci();
         }
         if(mainMenuChoice == 4){
+            qs->topicPhysics();
+        }
+        if(mainMenuChoice == 5){
+            qs->topicChemistry();
+        }
+        if(mainMenuChoice == 6){
+            qs->topicGeography();
+        }
+        if(mainMenuChoice == 7){
+            qs->topicHistory();
+        }
+        if(mainMenuChoice == 8){
+            qs->topicMath();
+        }
+        if(mainMenuChoice == 9){
+            qs->topicScience();
+        }
+        if(mainMenuChoice == 10){
             qs->topicRandomTrivia();
         }
-        if(mainMenuChoice == 5){ //standard exit message, ends the while loop, exits program.
+        if(mainMenuChoice == 11){ //standard exit message, ends the while loop, exits program.
             cout << "Thanks for playing!" << endl;
             powerSwitch = false;
             return 0;
