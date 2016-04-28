312910e8689b6f440e04ea465ec5026736d52bdc
bstoriz <bstoriz@gmail.com>
2016-04-27 19:36:36 -0600

    Fixed loops and added number of correct

--- quizShow.cpp
+++ quizShow.cpp
@@ -60,103 +60,102 @@ void quizShow::topicRandomTrivia(){
 
 //Reads in the right file with the given number of questions
 void quizShow::readFile(char *fileName, int numQuestions) {
-    topicQuestion questions[3];
+    topicQuestion tq;
+    string answers[3];
     ifstream inFile;
     string line;
     inFile.open(fileName);
     if (inFile.good()) {
-        while (!inFile.eof()) {
-            //Based off the number of questions, it will fill the struct with the proper questions and answers.
-            for (int i = 0; i < 3; i++) {
-                string question1, question2, question3, answer11, answer12, answer13, answer21, answer22, answer23, answer31, answer32, answer33, correctAnswer1, correctAnswer2, correctAnswer3;
-                //stuff for question1
-                getline(inFile, question1);
-                questions[i].question1 = question1;
+        string question1, question2, question3, answer11, answer12, answer13, answer21, answer22, answer23, answer31, answer32, answer33, correctAnswer1, correctAnswer2, correctAnswer3;
+        //stuff for question1
+        getline(inFile, question1);
+        tq.question1 = question1;
 
-                getline(inFile, answer11);
-                questions[i].answer11 = answer11;
+        getline(inFile, answer11);
+        tq.answer11 = answer11;
 
-                getline(inFile, answer12);
-                questions[i].answer12 = answer12;
+        getline(inFile, answer12);
+        tq.answer12 = answer12;
 
-                getline(inFile, answer13);
-                questions[i].answer13 = answer13;
+        getline(inFile, answer13);
+        tq.answer13 = answer13;
 
-                getline(inFile, correctAnswer1);
-                questions[i].correctAnswer1 = atoi(correctAnswer1.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
-                //stuff for question2
-                getline(inFile, question2);
-                questions[i].question2 = question2;
+        getline(inFile, correctAnswer1);
+        tq.correctAnswer1 = atoi(correctAnswer1.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
+        //stuff for question2
 
-                getline(inFile, answer21);
-                questions[i].answer21 = answer21;
+        getline(inFile, question2);
+        tq.question2 = question2;
 
-                getline(inFile, answer22);
-                questions[i].answer22 = answer22;
+        getline(inFile, answer21);
+        tq.answer21 = answer21;
 
-                getline(inFile, answer23);
-                questions[i].answer23 = answer23;
+        getline(inFile, answer22);
+        tq.answer22 = answer22;
 
-                getline(inFile, correctAnswer2);
-                questions[i].correctAnswer2 = atoi(correctAnswer2.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
-                //stuff for question3
-                getline(inFile, question3);
-                questions[i].question3 = question3;
+        getline(inFile, answer23);
+        tq.answer23 = answer23;
 
-                getline(inFile, answer31);
-                questions[i].answer31 = answer31;
+        getline(inFile, correctAnswer2);
+        tq.correctAnswer2 = atoi(correctAnswer2.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
 
-                getline(inFile, answer32);
-                questions[i].answer32 = answer32;
+        //stuff for question3
+        getline(inFile, question3);
+        tq.question3 = question3;
 
-                getline(inFile, answer33);
-                questions[i].answer33 = answer33;
+        getline(inFile, answer31);
+        tq.answer31 = answer31;
 
-                getline(inFile, correctAnswer3);
-                questions[i].correctAnswer3 = atoi(correctAnswer3.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
-            }
-            while(){//there are questions in the list
-                int userChoiceAnswer;
-                cout << questions[0].question1 << endl;
-                cout << questions[0].answer11 << endl;
-                cout << questions[0].answer12 << endl;
-                cout << questions[0].answer13 << endl;
-                cin.ignore();
-                cin >> userChoiceAnswer;
-                if(userChoiceAnswer == questions[0].correctAnswer1){
-                    //delete from list
-                }
-                else{
-                    //move wrong answer to end of list
-                }
+        getline(inFile, answer32);
+        tq.answer32 = answer32;
 
-                cout << questions[0].question2 << endl;
-                cout << questions[0].answer21 << endl;
-                cout << questions[0].answer22 << endl;
-                cout << questions[0].answer23 << endl;
-                cin.ignore();
-                cin >> userChoiceAnswer;
-                if(userChoiceAnswer == questions[0].correctAnswer2){
-                    //delete from list
-                }
-                else{
-                    //move wrong answer to end of list
-                }
-                cout << questions[0].question3 << endl;
-                cout << questions[0].answer31 << endl;
-                cout << questions[0].answer32 << endl;
-                cout << questions[0].answer33 << endl;
-                cin.ignore();
-                cin >> userChoiceAnswer;
-                if(userChoiceAnswer == questions[0].correctAnswer3){
-                    //delete from list
-                }
-                else{
-                    //move wrong answer to end of list
-                }
-            }
-            inFile.close();
+        getline(inFile, answer33);
+        tq.answer33 = answer33;
+
+        getline(inFile, correctAnswer3);
+        tq.correctAnswer3 = atoi(correctAnswer3.c_str()); //int answer = atoi(token1.c_str()); //atoi vs stoi to be more accessible to people not using c++ 11
+
+        int userChoiceAnswer;
+        cout << tq.question1 << endl;
+        cout << tq.answer11 << endl;
+        cout << tq.answer12 << endl;
+        cout << tq.answer13 << endl;
+        //cin.ignore();
+        cin >> userChoiceAnswer;
+        answers[0] = userChoiceAnswer;
+
+        if(userChoiceAnswer == tq.correctAnswer1){
+            //delete from list
+            numberOfCorrect++;
         }
+
+        cout << tq.question2 << endl;
+        cout << tq.answer21 << endl;
+        cout << tq.answer22 << endl;
+        cout << tq.answer23 << endl;
+       // cin.ignore();
+        cin >> userChoiceAnswer;
+        answers[1] = userChoiceAnswer;
+        if(userChoiceAnswer == tq.correctAnswer2){
+            //delete from list
+            numberOfCorrect++;
+        }
+
+        cout << tq.question3 << endl;
+        cout << tq.answer31 << endl;
+        cout << tq.answer32 << endl;
+        cout << tq.answer33 << endl;
+        //cin.ignore();
+        cin >> userChoiceAnswer;
+        answers[2] = userChoiceAnswer;
+        if(userChoiceAnswer == tq.correctAnswer3){
+            //delete from list
+            numberOfCorrect++;
+        }
+        inFile.close();
+
+        cout << "You got " << numberOfCorrect << " answers correct." << endl;
+
     }
     else {
         cout << "Could not read file." << endl;
